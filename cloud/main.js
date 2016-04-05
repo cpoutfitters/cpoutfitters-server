fb517b052aa15d11cadc12604388d20bf2cad01b
Eric Gonzalez <erigonz89@gmail.com>
2016-04-04 21:25:39 -0500

    Added outfit recommend endpoint

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,63 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+var Outfit = Parse.Object.extend("Outfit");
+
+Parse.Cloud.define('outfits/recommend', function(req, res) {
+
+  var occasion = req.params.occasion;
+  var owner = req.params.owner;
+
+  var tQuery = new Parse.tQuery("Article");
+  tQuery.equalTo("type", "top");
+  tQuery.equalTo("occasion", occasion);
+  tQuery.equalTo("owner", owner);
+  tQuery.ascending("lastWorn");
+
+  // This tQuery.find() is unlikely to finish before response.success() is called.
+  tQuery.first({
+  success: function(top) {
+    // Successfully retrieved the object.
+    var bQuery = new Parse.tQuery("Article");
+    bQuery.equalTo("type", "bottom");
+    bQuery.equalTo("occasion", occasion);
+    bQuery.equalTo("owner", owner);
+    bQuery.ascending("lastWorn");
+
+    // This tQuery.find() is unlikely to finish before response.success() is called.
+    bQuery.first({
+    success: function(bottom) {
+
+      // Successfully retrieved the object.
+      var fQuery = new Parse.tQuery("Article");
+      fQuery.equalTo("type", "footwear");
+      fQuery.equalTo("occasion", occasion);
+      fQuery.equalTo("owner", owner);
+      fQuery.ascending("lastWorn");
+
+      // This tQuery.find() is unlikely to finish before response.success() is called.
+      bQuery.first({
+      success: function(footwear) {
+
+        // Successfully retrieved the object.
+        var outfit = new Outfit();
+        outfit.owner = owner;
+        outfit.components = [top, bottom, footwear];
+        outfit.lastWorn = new Date();
+        outfit.useCount = 0;
+
+        response.success(outfit); // Response: "<Outfit>"
+      },
+      error: function(error) {
+        res.error("No footwear found")
+      }
+    },
+    error: function(error) {
+      alert("Error: " + error.code + " " + error.message);
+    }
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
