2a3a17aee20c7a10d1bcef4aca38f6eefb6f5b68
Eric Gonzalez <erigonz89@gmail.com>
2016-04-06 21:22:53 -0500

    Fixed typos

--- cloud/main.js
+++ cloud/main.js
@@ -33,14 +33,14 @@ Parse.Cloud.define('recommend', function (req, res) {
             // This tQuery.find() is unlikely to finish before response.success() is called.
             bQuery.find({
                 success: function (results) {
-                    if (results.length == 0) {
+                    if (results.length === 0) {
                         console.log("No bottoms, cannot compile outfit");
                         res.error("No bottoms available");
                         return;
                     }
 
                     var bottom = results[0];
-                    console.log("Found a bottom!")
+                    console.log("Found a bottom!");
                     // Successfully retrieved the object.
                     var fQuery = new Parse.tQuery("Article");
                     fQuery.equalTo("type", "footwear");
@@ -51,14 +51,14 @@ Parse.Cloud.define('recommend', function (req, res) {
                     // This tQuery.find() is unlikely to finish before response.success() is called.
                     fQuery.find({
                         success: function (results) {
-                            if (results.length == 0) {
+                            if (results.length === 0) {
                                 console.log("No footwear, cannot compile outfit");
                                 res.error("No footwear available");
                                 return;
                             }
 
                             var footwear = results[0];
-                            console.log("Found a footwear!")
+                            console.log("Found a footwear!");
                             // Successfully retrieved the object.
                             var Outfit = Parse.Object.extend("Outfit"), outfit = new Outfit();
                             outfit.owner = owner;
