578ec9a8e03898d20c4ccbd093dcd507ebf7f9f1
Eric Gonzalez <erigonz89@gmail.com>
2016-04-06 22:39:04 -0500

    moved around creation and logging

--- cloud/main.js
+++ cloud/main.js
@@ -4,28 +4,19 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define('recommend', function (req, res) {
-    console.log(req.params);
     var occasion = req.params.occasion, owner = req.user, tQuery = new Parse.Query("Article");
     tQuery.equalTo("type", "top");
     tQuery.equalTo("occasion", occasion);
     // tQuery.equalTo("owner", owner);
     tQuery.ascending("lastWorn");
 
-    var Outfit = Parse.Object.extend("Outfit"), outfit = new Outfit();
-    outfit.owner = owner;
-
-    console.log(tQuery);
     // This tQuery.find() is unlikely to finish before response.success() is called.
     tQuery.first().then(function (top) {
         if (top === undefined) {
-            console.log("No tops, cannot compile outfit");
-            res.error("No tops available");
+            res.success(null);
             return;
         }
 
-        outfit.topComponent = top;
-
-        console.log("Found a top!");
         // Successfully retrieved the object.
         var bQuery = new Parse.Query("Article");
         bQuery.equalTo("type", "bottom");
@@ -34,18 +25,12 @@ Parse.Cloud.define('recommend', function (req, res) {
         bQuery.ascending("lastWorn");
 
         // This tQuery.find() is unlikely to finish before response.success() is called.
-        console.log(bQuery);
-
         bQuery.first().then(function (bottom) {
             if (bottom === undefined) {
-                console.log("No bottoms, cannot compile outfit");
-                res.error("No bottoms available");
+                res.success(null);
                 return;
             }
 
-            outfit.bottomComponent = bottom;
-
-            console.log("Found a bottom!");
             // Successfully retrieved the object.
             var fQuery = new Parse.Query("Article");
             fQuery.equalTo("type", 'footwear');
@@ -54,22 +39,23 @@ Parse.Cloud.define('recommend', function (req, res) {
             fQuery.ascending("lastWorn");
 
             // This tQuery.find() is unlikely to finish before response.success() is called.
-            console.log(fQuery);
             fQuery.first().then(function (footwear) {
                 if (footwear === undefined) {
-                    console.log("No footwear, cannot compile outfit");
-                    res.error("No footwear available");
+                    res.success(null);
                     return;
                 }
 
+                var Outfit = Parse.Object.extend("Outfit"), outfit = new Outfit();
+                outfit.owner = owner;
+                outfit.topComponent = top;
+                outfit.bottomComponent = bottom;
                 outfit.footwearComponent = footwear;
-                console.log("Found a footwear!");
                 // Successfully retrieved the object.
                 outfit.lastWorn = new Date();
                 outfit.useCount = 0;
 
-                console.log(outfit);
-                res.success(JSON.stringify(outfit)); // Response: "<Outfit>"
+                console.log("Returning outfit: " + outfit);
+                res.success(outfit); // Response: "<Outfit>"
             });
         });
     });
