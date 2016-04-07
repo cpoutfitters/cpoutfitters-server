a2897b9303aee05caaa001ec55d7598c2c1ec45a
Eric Gonzalez <erigonz89@gmail.com>
2016-04-06 22:24:43 -0500

    no owner for you

--- cloud/main.js
+++ cloud/main.js
@@ -25,51 +25,51 @@ Parse.Cloud.define('recommend', function (req, res) {
 
         outfit.topComponent = top;
         res.success(JSON.stringify(outfit))
-        //
-        // console.log("Found a top!");
-        // // Successfully retrieved the object.
-        // var bQuery = new Parse.tQuery("Article");
-        // bQuery.equalTo("type", "bottom");
+
+        console.log("Found a top!");
+        // Successfully retrieved the object.
+        var bQuery = new Parse.tQuery("Article");
+        bQuery.equalTo("type", "bottom");
         // bQuery.equalTo("occasion", occasion);
-        // bQuery.equalTo("owner", owner);
-        // bQuery.ascending("lastWorn");
-        //
-        // // This tQuery.find() is unlikely to finish before response.success() is called.
-        // console.log(bQuery);
-        //
-        // bQuery.first().then(function (bottom) {
-        //     if (bottom === undefined) {
-        //         console.log("No bottoms, cannot compile outfit");
-        //         res.error("No bottoms available");
-        //         return;
-        //     }
-        //
-        //     console.log("Found a bottom!");
-        //     // Successfully retrieved the object.
-        //     var fQuery = new Parse.tQuery("Article");
-        //     fQuery.equalTo("type", 'footwear');
-        //     fQuery.equalTo("occasion", occasion);
-        //     fQuery.equalTo("owner", owner);
-        //     fQuery.ascending("lastWorn");
-        //
-        //     // This tQuery.find() is unlikely to finish before response.success() is called.
-        //     console.log(fQuery);
-        //     fQuery.first().then(function (footwear) {
-        //         if (footwear === undefined) {
-        //             console.log("No footwear, cannot compile outfit");
-        //             res.error("No footwear available");
-        //             return;
-        //         }
-        //
-        //         console.log("Found a footwear!");
-        //         // Successfully retrieved the object.
-        //         outfit.components = [top, bottom, footwear];
-        //         outfit.lastWorn = new Date();
-        //         outfit.useCount = 0;
-        //
-        //         console.log(outfit);
-        //         res.success(JSON.stringify(outfit)); // Response: "<Outfit>"
-        //     });
-        // });
+        bQuery.equalTo("owner", owner);
+        bQuery.ascending("lastWorn");
+
+        // This tQuery.find() is unlikely to finish before response.success() is called.
+        console.log(bQuery);
+
+        bQuery.first().then(function (bottom) {
+            if (bottom === undefined) {
+                console.log("No bottoms, cannot compile outfit");
+                res.error("No bottoms available");
+                return;
+            }
+
+            console.log("Found a bottom!");
+            // Successfully retrieved the object.
+            var fQuery = new Parse.tQuery("Article");
+            fQuery.equalTo("type", 'footwear');
+            // fQuery.equalTo("occasion", occasion);
+            fQuery.equalTo("owner", owner);
+            fQuery.ascending("lastWorn");
+
+            // This tQuery.find() is unlikely to finish before response.success() is called.
+            console.log(fQuery);
+            fQuery.first().then(function (footwear) {
+                if (footwear === undefined) {
+                    console.log("No footwear, cannot compile outfit");
+                    res.error("No footwear available");
+                    return;
+                }
+
+                console.log("Found a footwear!");
+                // Successfully retrieved the object.
+                outfit.components = [top, bottom, footwear];
+                outfit.lastWorn = new Date();
+                outfit.useCount = 0;
+
+                console.log(outfit);
+                res.success(JSON.stringify(outfit)); // Response: "<Outfit>"
+            });
+        });
     });
 });
