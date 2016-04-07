e04d9ad84c1e2a8fadf09999051c54740a5fc55c
Eric Gonzalez <erigonz89@gmail.com>
2016-04-06 22:50:29 -0500

    logging output

--- cloud/main.js
+++ cloud/main.js
@@ -14,49 +14,52 @@ Parse.Cloud.define('recommend', function (req, res) {
     tQuery.first().then(function (top) {
         if (top === undefined) {
             res.success(null);
-            return;
         }
-
-        // Successfully retrieved the object.
-        var bQuery = new Parse.Query("Article");
-        bQuery.equalTo("type", "bottom");
-        // bQuery.equalTo("occasion", occasion);
-        bQuery.equalTo("owner", owner);
-        bQuery.ascending("lastWorn");
-
-        // This tQuery.find() is unlikely to finish before response.success() is called.
-        bQuery.first().then(function (bottom) {
-            if (bottom === undefined) {
-                res.success(null);
-                return;
-            }
-
+        else {
             // Successfully retrieved the object.
-            var fQuery = new Parse.Query("Article");
-            fQuery.equalTo("type", 'footwear');
-            // fQuery.equalTo("occasion", occasion);
-            fQuery.equalTo("owner", owner);
-            fQuery.ascending("lastWorn");
+            var bQuery = new Parse.Query("Article");
+            bQuery.equalTo("type", "bottom");
+            // bQuery.equalTo("occasion", occasion);
+            bQuery.equalTo("owner", owner);
+            bQuery.ascending("lastWorn");
 
             // This tQuery.find() is unlikely to finish before response.success() is called.
-            fQuery.first().then(function (footwear) {
-                if (footwear === undefined) {
+            bQuery.first().then(function (bottom) {
+                if (bottom === undefined) {
                     res.success(null);
                     return;
                 }
+                else {
+                    // Successfully retrieved the object.
+                    var fQuery = new Parse.Query("Article");
+                    fQuery.equalTo("type", 'footwear');
+                    // fQuery.equalTo("occasion", occasion);
+                    fQuery.equalTo("owner", owner);
+                    fQuery.ascending("lastWorn");
 
-                var Outfit = Parse.Object.extend("Outfit"), outfit = new Outfit();
-                outfit.owner = owner;
-                outfit.topComponent = top;
-                outfit.bottomComponent = bottom;
-                outfit.footwearComponent = footwear;
-                // Successfully retrieved the object.
-                outfit.lastWorn = new Date();
-                outfit.useCount = 0;
+                    // This tQuery.find() is unlikely to finish before response.success() is called.
+                    var top = fQuery.first().then(function (footwear) {
+                        if (footwear === undefined) {
+                            res.success(null);
+                            return;
+                        }
+                        else {
+                            var Outfit = Parse.Object.extend("Outfit"), outfit = new Outfit();
+                            outfit.owner = owner;
+                            outfit.topComponent = top;
+                            outfit.bottomComponent = bottom;
+                            outfit.footwearComponent = footwear;
+                            // Successfully retrieved the object.
+                            outfit.lastWorn = new Date();
+                            outfit.useCount = 0;
 
-                console.log("Returning outfit: " + outfit);
-                res.success(outfit); // Response: "<Outfit>"
+                            var output = JSON.stringify(outfit);
+                            console.log("Returning outfit: " + outfit);
+                            res.success(output); // Response: "<Outfit>"
+                        }
+                    });
+                }
             });
-        });
+        }
     });
 });
