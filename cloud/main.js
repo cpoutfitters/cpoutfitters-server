fffbd9e192157f8469684a83fbaa5f51bb48d3ac
Eric Gonzalez <erigonz89@gmail.com>
2016-04-06 23:17:10 -0500

    saving outfit first

--- cloud/main.js
+++ cloud/main.js
@@ -54,19 +54,29 @@ Parse.Cloud.define('recommend', function (req, res) {
                             outfit.useCount = 0;
 
                             outfit.save().then(function (results) {
-                                console.log("Outfit: " + results);
-                                var json = results.toJSON();
-                                console.log("JSON: " + json);
-                                var output = JSON.stringify(json);
-                                console.log("Returning string: " + output);
-                                res.success(results); // Response: "<Outfit>"
-                            }, function (error) {
-                                console.log(error);
-                            });
-                        }
-                    });
-                }
-            });
-        }
-    });
+                                var id = results.objectId;
+                                var outfitQuery = Parse.Query("Outfit");
+                                outfitQuery.equalTo("objectId", id);
+                                outfitQuery.find().then(function (outfit) {
+                                    console.log("Outfit: " + results);
+                                    var json = results.toJSON();
+                                    console.log("JSON: " + json);
+                                    var output = JSON.stringify(json);
+                                    console.log("Returning string: " + output);
+                                    res.success(results); // Response: "<Outfit>"
+
+                                },
+                                function (error) {
+                                    console.log(error);
+                                }
+                            );
+                        }, function (error) {
+                            console.log(error);
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
 });
