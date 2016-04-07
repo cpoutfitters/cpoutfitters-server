43d46c1e2a59efbaa6f8cf912f24c5e3f836f4bb
Eric Gonzalez <erigonz89@gmail.com>
2016-04-06 22:13:20 -0500

    Breaking more promises

--- cloud/main.js
+++ cloud/main.js
@@ -16,23 +16,15 @@ Parse.Cloud.define('recommend', function (req, res) {
 
     console.log(tQuery);
     // This tQuery.find() is unlikely to finish before response.success() is called.
-    tQuery.first({
-        success: function (top) {
-            if (top === undefined) {
-                console.log("No tops, cannot compile outfit");
-                res.error("No tops available");
-                return;
-            }
-
-            console.log("Found a top!");
-            outfit.topComponent = top;
-            // Successfully retrieved the object.
-        },
-        error: function (error) {
-            console.log("Error: " + error.code + " " + error.message);
-            res.error("No tops found");
+    tQuery.first().then(function (top) {
+        if (top === undefined) {
+            console.log("No tops, cannot compile outfit");
+            res.error("No tops available");
+            return;
         }
-    }).then(function(something) {
+
+        console.log("Found a top!");
+        // Successfully retrieved the object.
         var bQuery = new Parse.tQuery("Article");
         bQuery.equalTo("type", "bottom");
         bQuery.equalTo("occasion", occasion);
@@ -42,51 +34,39 @@ Parse.Cloud.define('recommend', function (req, res) {
         // This tQuery.find() is unlikely to finish before response.success() is called.
         console.log(bQuery);
 
-        bQuery.first({
-            success: function (bottom) {
-                if (bottom === undefined) {
-                    console.log("No bottoms, cannot compile outfit");
-                    res.error("No bottoms available");
+        bQuery.first().then(function (bottom) {
+            if (bottom === undefined) {
+                console.log("No bottoms, cannot compile outfit");
+                res.error("No bottoms available");
+                return;
+            }
+
+            console.log("Found a bottom!");
+            // Successfully retrieved the object.
+            var fQuery = new Parse.tQuery("Article");
+            fQuery.equalTo("type", 'footwear');
+            fQuery.equalTo("occasion", occasion);
+            fQuery.equalTo("owner", owner);
+            fQuery.ascending("lastWorn");
+
+            // This tQuery.find() is unlikely to finish before response.success() is called.
+            console.log(fQuery);
+            fQuery.first().then(function (footwear) {
+                if (footwear === undefined) {
+                    console.log("No footwear, cannot compile outfit");
+                    res.error("No footwear available");
                     return;
                 }
 
-                console.log("Found a bottom!");
+                console.log("Found a footwear!");
                 // Successfully retrieved the object.
-                var fQuery = new Parse.tQuery("Article");
-                fQuery.equalTo("type", 'footwear');
-                fQuery.equalTo("occasion", occasion);
-                fQuery.equalTo("owner", owner);
-                fQuery.ascending("lastWorn");
+                outfit.components = [top, bottom, footwear];
+                outfit.lastWorn = new Date();
+                outfit.useCount = 0;
 
-                // This tQuery.find() is unlikely to finish before response.success() is called.
-                console.log(fQuery);
-                fQuery.first({
-                    success: function (footwear) {
-                        if (footwear === undefined) {
-                            console.log("No footwear, cannot compile outfit");
-                            res.error("No footwear available");
-                            return;
-                        }
-
-                        console.log("Found a footwear!");
-                        // Successfully retrieved the object.
-                        outfit.components = [top, bottom, footwear];
-                        outfit.lastWorn = new Date();
-                        outfit.useCount = 0;
-
-                        console.log(outfit);
-                        res.success(JSON.stringify(outfit)); // Response: "<Outfit>"
-                    },
-                    error: function (error) {
-                        console.log("Error: " + error.code + " " + error.message);
-                        res.error("No footwear found");
-                    }
-                });
-            },
-            error: function (error) {
-                console.log("Error: " + error.code + " " + error.message);
-                res.error("No bottoms found");
-            }
+                console.log(outfit);
+                res.success(JSON.stringify(outfit)); // Response: "<Outfit>"
+            });
         });
     });
 });
